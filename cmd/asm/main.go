8332488b7c0bba29a489716205cb01d76ba12c77
Daker Fernandes Pinheiro <dfp@cin.ufpe.br>
2017-06-16 11:10:42 -0300

    split main in a different file

---
+++ cmd/asm/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"io"
+	"os"
+)
+
+var (
+	outputFileFlag = flag.String("o", "a.bin", "output Parsed object")
+	dumpFlag       = flag.Bool("d", false, "output hex dump format instead of binary")
+	stdoutFlag     = flag.Bool("stdout", false, "output into stdout")
+	txtFlag        = flag.Bool("txt", false, "output txt")
+	zeroPadFlag    = flag.Int("pad", -1, "zero padding size")
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		panic("no input file given")
+	}
+
+	r, err := os.Open(args[0])
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	obj, err := Parse(r)
+	if err != nil {
+		panic(err)
+	}
+
+	var w io.WriteCloser
+	switch {
+	case *stdoutFlag:
+		w = os.Stdout
+	case *outputFileFlag != "":
+		f, err := os.OpenFile(*outputFileFlag, os.O_CREATE|os.O_WRONLY, 0755)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		w = f
+	default:
+		panic("no output set")
+	}
+
+	if *zeroPadFlag > 0 {
+		w = &ZeroPad{w, *zeroPadFlag}
+	}
+
+	if *dumpFlag {
+		w = hex.Dumper(w)
+		defer w.Close()
+	}
+
+	var assemble Assembler
+	if *txtFlag {
+		assemble = AssembleText
+	} else {
+		assemble = AssembleBinary
+	}
+	if err := assemble(w, obj); err != nil {
+		panic(err)
+	}
+}
--- cmd/asm/riscv.go
+++ cmd/asm/riscv.go
@@ -3,12 +3,9 @@ package main
 import (
 	"bufio"
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
-	"flag"
 	"fmt"
 	"io"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -262,66 +259,4 @@ func AssembleText(w io.Writer, o *Object) error {
 		}
 	}
 	return nil
-}
-
-var (
-	outputFileFlag = flag.String("o", "a.bin", "output Parsed object")
-	dumpFlag       = flag.Bool("d", false, "output hex dump format instead of binary")
-	stdoutFlag     = flag.Bool("stdout", false, "output into stdout")
-	txtFlag        = flag.Bool("txt", false, "output txt")
-	zeroPadFlag    = flag.Int("pad", -1, "zero padding size")
-)
-
-func main() {
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) == 0 {
-		panic("no input file given")
-	}
-
-	r, err := os.Open(args[0])
-	if err != nil {
-		panic(err)
-	}
-	defer r.Close()
-
-	obj, err := Parse(r)
-	if err != nil {
-		panic(err)
-	}
-
-	var w io.WriteCloser
-	switch {
-	case *stdoutFlag:
-		w = os.Stdout
-	case *outputFileFlag != "":
-		f, err := os.OpenFile(*outputFileFlag, os.O_CREATE|os.O_WRONLY, 0755)
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-		w = f
-	default:
-		panic("no output set")
-	}
-
-	if *zeroPadFlag > 0 {
-		w = &ZeroPad{w, *zeroPadFlag}
-	}
-
-	if *dumpFlag {
-		w = hex.Dumper(w)
-		defer w.Close()
-	}
-
-	var assemble Assembler
-	if *txtFlag {
-		assemble = AssembleText
-	} else {
-		assemble = AssembleBinary
-	}
-	if err := assemble(w, obj); err != nil {
-		panic(err)
-	}
 }
